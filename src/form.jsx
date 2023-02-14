8c91f40f16dfc5c9861819b8882f56852a9a2072
Westhly Sarabia <westhly-carrillo@outlook.com>
2023-02-14 12:55:33 -0500

    variables creadas

--- src/form.jsx
+++ src/form.jsx
@@ -3,12 +3,42 @@ import  React  from "react";
 function Formulario () {
 
     return (
+        
         <form>
+            <h1>Buenas noches: Tienda 0707 Mayales</h1>
   <label>
-    Name:
-    <input type="text" name="name" />
+    Presupuesto:
+    <input type="number" name="num" />
   </label>
-  <input type="submit" value="Submit" />
+  <label>
+    Venta sin IVA:
+    <input type="number" name="name" />
+  </label>
+  <label>
+    Cumplimiento:
+    <input type="number" name="name" />
+  </label>
+  <label>
+    LFL:
+    <input type="number" name="name" />
+  </label>
+  <h2>QUIEBRAS</h2>
+  <label>
+    Liquidacion:
+    <input type="number" name="name" />
+  </label>
+  <label>
+    Liquidacion:
+    <input type="number" name="name" />
+    <input type="number" name="porcentaje" />
+  </label>
+  <label>
+    Ajustes:
+    <input type="number" name="name" />
+    <input type="number" name="porcentaje" />
+  </label>
+
+  <input type="submit" value="Imprimir" />
 </form>
     )
    
