1772058e1f0e1f83d7a7073e7d185733e077c8da
Westhly Sarabia <westhly-carrillo@outlook.com>
2023-02-15 17:36:19 -0500

    aplicando UseState

--- src/form.jsx
+++ src/form.jsx
@@ -1,40 +1,51 @@
 import  React  from "react";
+import { useState } from 'react';
+
 
 function Formulario () {
 
+  const [presupuesto, setPresupuesto] = useState('');
+  const [ventaiva, setVentaiva] = useState('');
+  const [cumplimiento, setCumplimineto] = useState('');
+  const [lfl, setLfL] = useState('');
+  const [liquidacion, setLiquidacion] = useState('');
+  const [ajustes, setAjustes] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault ();
+    console.log('Presupuesto:', presupuesto, 'Venta sin IVA:', ventaiva, 'Cumplimiento:', cumplimiento, 'LFL:', lfl, 'Liquidacion', liquidacion, 'Ajustes:', ajustes);
+  }
+
+
     return (
         
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Buenas noches: Tienda 0707 Mayales</h1>
-  <label>
+          <label>
     Presupuesto:
-    <input type="number" name="num" />
+    <input type="number" value={presupuesto} onChange={(event) => setPresupuesto(event.target.value)} />
   </label>
   <label>
     Venta sin IVA:
-    <input type="number" name="name" />
+    <input type="number" value={ventaiva} onChange={(event) => setVentaiva(event.target.value)} />
   </label>
   <label>
     Cumplimiento:
-    <input type="number" name="name" />
+    <input type="number" value={cumplimiento} onChange={(event) => setCumplimineto(event.target.value)} />
   </label>
   <label>
     LFL:
-    <input type="number" name="name" />
+    <input type="number" value={lfl} onChange={(event) => setLfL(event.target.value)} />
   </label>
   <h2>QUIEBRAS</h2>
   <label>
     Liquidacion:
-    <input type="number" name="name" />
-  </label>
-  <label>
-    Liquidacion:
-    <input type="number" name="name" />
+    <input type="number" value={liquidacion} onChange={(event) => setLiquidacion(event.target.value)} />
     <input type="number" name="porcentaje" />
   </label>
   <label>
     Ajustes:
-    <input type="number" name="name" />
+    <input type="number" value={ajustes} onChange={(event) => setAjustes(event.target.value)} />
     <input type="number" name="porcentaje" />
   </label>
 
